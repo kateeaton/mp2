092fca9684a915fe38b808dc8027c07def818cb9
Birgit Alitz <balitz@trumotion.net>
2017-10-27 16:28:36 -0500

    el wuz here

--- src/Flow.java
+++ src/Flow.java
@@ -54,152 +54,92 @@ public class Flow {
         }
         return assignment.get(0).get(0);
     }
+
+
     public boolean isValid(Character value, CSP current, ArrayList<ArrayList<CSP>> assignment, Integer size) {
         boolean retVal = true;
         boolean r = false;
         boolean l = false;
         boolean u = false;
         boolean d = false;
-        CSP right = current;
-        CSP left = current;
-        CSP up = current;
-        CSP down = current;
+        CSP right = new CSP();
+        CSP left = new CSP();
+        CSP up = new CSP();
+        CSP down = new CSP();
+        right = current;
+        left = current;
+        up = current;
+        down = current;
+
         Integer x = current.x;
         Integer y = current.y;
-        if (x - 1 > 0) {
+        if (x - 1 >= 0) {
             left = assignment.get(x - 1).get(y);
             l = true;
         }
-        if (y - 1 > 0) {
+        if (y - 1 >= 0) {
             up = assignment.get(x).get(y - 1);
             u = true;
         }
-        if (x + 1 < size) {
+        if (x + 1 <= size) {
             right = assignment.get(x + 1).get(y);
             r = true;
         }
-        if (y + 1 < size) {
+        if (y + 1 <= size) {
             down = assignment.get(x).get(y + 1);
             d = true;
         }
 
         if(l && r && u && d) {
-            if(left.getValue() == value && (right.getValue() != value && up.getValue() != value && down.getValue() != value)) {
-                return true;
-            }
-            else if(right.getValue() == value && (left.getValue() != value && up.getValue() != value && down.getValue() != value)) {
-                return true;
-            }
-            else if(up.getValue() == value && (right.getValue() != value && left.getValue() != value && down.getValue() != value)) {
-                return true;
-            }
-            else if(down.getValue() == value && (left.getValue() != value && up.getValue() != value && right.getValue() != value)) {
-                return true;
-            }
-            else{
-                return false;
-            }
-
+            if(left.getValue() == value && (right.getValue() != value && up.getValue() != value && down.getValue() != value)) { return true; }
+            else if(right.getValue() == value && (left.getValue() != value && up.getValue() != value && down.getValue() != value)) { return true; }
+            else if(up.getValue() == value && (right.getValue() != value && left.getValue() != value && down.getValue() != value)) { return true; }
+            else if(down.getValue() == value && (left.getValue() != value && up.getValue() != value && right.getValue() != value)) { return true; }
+            else{ return false; }
         }
-        else if( l && u && d) {
-            if(left.getValue() == value && (up.getValue() != value && down.getValue() != value)) {
-                return true;
-            }
-            else if(up.getValue() == value && (left.getValue() != value && down.getValue() != value)) {
-                return true;
-            }
-            else if(down.getValue() == value && (left.getValue() != value && up.getValue() != value )){
-                return true;
-            }
-            else{
-                return false;
-            }
+        else if(l && u && d) {
+            if(left.getValue() == value && (up.getValue() != value && down.getValue() != value)) { return true; }
+            else if(up.getValue() == value && (left.getValue() != value && down.getValue() != value)){ return true; }
+            else if(down.getValue() == value && (left.getValue() != value && up.getValue() != value )){ return true; }
+            else{ return false; }
         }
         else if(r && u && d) {
-            if(right.getValue() == value && (up.getValue() != value && down.getValue() != value)) {
-                return true;
-            }
-            else if(up.getValue() == value && (right.getValue() != value && down.getValue() != value)) {
-                return true;
-            }
-            else if(down.getValue() == value && (right.getValue() != value && up.getValue() != value )){
-                return true;
-            }
-            else{
-                return false;
-            }
+            if(right.getValue() == value && (up.getValue() != value && down.getValue() != value)) { return true; }
+            else if(up.getValue() == value && (right.getValue() != value && down.getValue() != value)) { return true; }
+            else if(down.getValue() == value && (right.getValue() != value && up.getValue() != value )){ return true; }
+            else{ return false; }
         }
         else if(l && r && d) {
-            if(left.getValue() == value && (right.getValue() != value && down.getValue() != value)) {
-                return true;
-            }
-            else if(right.getValue() == value && (left.getValue() != value && down.getValue() != value)) {
-                return true;
-            }
-            else if(down.getValue() == value && (left.getValue() != value && right.getValue() != value )){
-                return true;
-            }
-            else{
-                return false;
-            }
+            if(left.getValue() == value && (right.getValue() != value && down.getValue() != value)) { return true; }
+            else if(right.getValue() == value && (left.getValue() != value && down.getValue() != value)) { return true; }
+            else if(down.getValue() == value && (left.getValue() != value && right.getValue() != value )){ return true; }
+            else{ return false; }
         }
         else if(l && r && u) {
-            if(left.getValue() == value && (up.getValue() != value && right.getValue() != value)) {
-                return true;
-            }
-            else if(up.getValue() == value && (left.getValue() != value && right.getValue() != value)) {
-                return true;
-            }
-            else if(right.getValue() == value && (left.getValue() != value && up.getValue() != value )){
-                return true;
-            }
-            else{
-                return false;
-            }
+            if(left.getValue() == value && (up.getValue() != value && right.getValue() != value)) { return true; }
+            else if(up.getValue() == value && (left.getValue() != value && right.getValue() != value)) { return true; }
+            else if(right.getValue() == value && (left.getValue() != value && up.getValue() != value )){ return true; }
+            else{ return false; }
         }
         else if(l && d) {
-            if(left.getValue() == value && (down.getValue() != value)) {
-                return true;
-            }
-            else if(down.getValue() == value && (left.getValue() != value)){
-                return true;
-            }
-            else{
-                return false;
-            }
+            if(left.getValue() == value && (down.getValue() != value)) { return true; }
+            else if(down.getValue() == value && (left.getValue() != value)){ return true; }
+            else{ return false; }
         }
         else if(l && u) {
-            if(left.getValue() == value && (up.getValue() != value)) {
-                return true;
-            }
-            else if(up.getValue() == value && (left.getValue() != value)){
-                return true;
-            }
-            else{
-                return false;
-            }
+            if(left.getValue() == value && (up.getValue() != value)) { return true; }
+            else if(up.getValue() == value && (left.getValue() != value)){ return true; }
+            else{ return false; }
         }
         else if(r && u) {
-            if(right.getValue() == value && (up.getValue() != value)) {
-                return true;
-            }
-            else if(up.getValue() == value && (right.getValue() != value)){
-                return true;
-            }
-            else{
-                return false;
-            }
+            if(right.getValue() == value && (up.getValue() != value)) { return true; }
+            else if(up.getValue() == value && (right.getValue() != value)){ return true; }
+            else{ return false; }
         }
         else if(r && d) {
-            if(right.getValue() == value && (down.getValue() != value)) {
-                return true;
-            }
-            else if(down.getValue() == value && (right.getValue() != value)){
-                return true;
-            }
-            else{
-                return false;
-            }
+            if(right.getValue() == value && (down.getValue() != value)) { return true; }
+            else if(down.getValue() == value && (right.getValue() != value)){ return true; }
+            else{ return false; }
         }
         return retVal;
     }
